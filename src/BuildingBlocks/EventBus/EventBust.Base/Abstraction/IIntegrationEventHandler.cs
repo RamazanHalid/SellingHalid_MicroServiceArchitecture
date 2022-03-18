649bea2ae25103055a6d50bcf534f236143b7538
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-18 13:21:15 +0300

    IIntegrationEventHandler created

---
+++ src/BuildingBlocks/EventBus/EventBust.Base/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using EventBust.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBust.Base.Abstraction
+{
+    public interface IIntegrationEventHandler<TIntegrationEvent> : IntegrationEventHandler where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IntegrationEventHandler
+    {
+    }
+
+}
