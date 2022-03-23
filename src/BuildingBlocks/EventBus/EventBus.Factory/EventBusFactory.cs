d593d9d1464815a580c9c636f5f6186a9134b76d
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-03-23 22:12:20 +0300

    EventBusRabbitMQ created

---
+++ src/BuildingBlocks/EventBus/EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,20 @@
+﻿using EventBus.AzureServiceBus;
+using EventBus.RabbitMQ;
+using EventBust.Base;
+using EventBust.Base.Abstraction;
+using System;
+
+namespace EventBus.Factory
+{
+    public static class EventBusFactory
+    {
+        public static IEventBus Create(EventBusConfig config, IServiceProvider serviceProvider)
+        {
+            return config.EventBusType switch
+            {
+                EventBusType.AzureServiceBus => new EventServiceBus(config, serviceProvider),
+                _ => new EventBusRabbitMQ(config, serviceProvider),
+            };
+        }
+    }
+}
