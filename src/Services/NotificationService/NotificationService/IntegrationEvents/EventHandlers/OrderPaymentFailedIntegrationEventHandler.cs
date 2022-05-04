92444b959ffe44194450027904acc2630149930a
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-05-05 00:14:07 +0300

    Notification service added

---
+++ src/Services/NotificationService/NotificationService/IntegrationEvents/EventHandlers/OrderPaymentFailedIntegrationEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using EventBus.Base.Abstraction;
+using Microsoft.Extensions.Logging;
+using NotificationService.IntegrationEvents.Events;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService.IntegrationEvents.EventHandlers
+{
+    class OrderPaymentFailedIntegrationEventHandler : IIntegrationEventHandler<OrderPaymentFailedIntegrationEvent>
+    {
+        private readonly ILogger<OrderPaymentFailedIntegrationEventHandler> _logger;
+
+        public OrderPaymentFailedIntegrationEventHandler(ILogger<OrderPaymentFailedIntegrationEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Handle(OrderPaymentFailedIntegrationEvent @event)
+        {
+            // Send Fail Notification (Sms, EMail, Push)
+
+            _logger.LogInformation($"Order Payment failed with OrderId: {@event.OrderId}, ErrorMessage: {@event.ErrorMessage}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
