e1119775180d38d127c3779581defb3fae9bf046
Ramazan Halid <ramazan.halid.35@gmail.com>
2022-05-13 21:38:04 +0300

    compose file updated

--- src/Services/BasketService/BasketService.Api/Startup.cs
+++ src/Services/BasketService/BasketService.Api/Startup.cs
@@ -65,7 +65,7 @@ namespace BasketService.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BasketService.Api v1"));
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseCors("CorsPolicy");
